34982f7fe37f72077889dac505a1eed7bb663829
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2025-11-21 09:39:48 -0300

    atualizando  aditivos nutritivos, seleção (get)

--- repositories/repo_aditivo_nutritivo.py
+++ repositories/repo_aditivo_nutritivo.py
@@ -1,5 +1,10 @@
 from conf.db_session import create_session
+from sqlalchemy import select  # , func
+
+from typing import Sequence
+
 from models.aditivo_nutritivo import AditivoNutritivo
+# from conf.helpers import formata_data
 
 
 def insert() -> AditivoNutritivo:
@@ -17,11 +22,40 @@ def insert() -> AditivoNutritivo:
         return an
 
 
-if __name__ == '__main__':
-    an = insert()
+# select * from aditivos_nutritivos
+def get_todos_aditivos_nutritivos() -> Sequence[AditivoNutritivo]:
+    with create_session() as session:
+        stmt = select(AditivoNutritivo)
+        result = session.execute(stmt)
+        return result.scalars().all()
 
-    print('Aditivo nutritivo cadastrado com sucesso!')
-    print(f'     ID: {an.id}')
-    print(f'   Data: {an.data_criacao}')
-    print(f'   Nome: {an.nome}')
-    print(f'Formula: {an.formula_quimica}')
+
+# select * from aditivos_nutritivos where id = 1
+def get_sabor_por_id(id_sabor: int) -> AditivoNutritivo | None:
+    with create_session() as session:
+        stmt = select(AditivoNutritivo).where(AditivoNutritivo.id == id_sabor)
+        result = session.execute(stmt)
+        return result.scalars().first()
+
+
+if __name__ == '__main__':
+    # # Inserindo dados
+    # an = insert()
+
+    # print('Aditivo nutritivo cadastrado com sucesso!')
+    # print(f'     ID: {an.id}')
+    # print(f'   Data: {an.data_criacao}')
+    # print(f'   Nome: {an.nome}')
+    # print(f'Formula: {an.formula_quimica}')
+
+    # # Seleciona todos
+    # an = get_todos_aditivos_nutritivos()
+    # for i in an:
+    #     print(f'ID: {i.id}, nome: {i.nome}, formula: {i.formula_quimica}')
+
+    # Seleciona por id
+    an = get_sabor_por_id(id_sabor=10)
+    if an is not None:
+        print(f'ID: {an.id}, nome: {an.nome}, formula: {an.formula_quimica}')
+    else:
+        print('Registro não encontrado')
